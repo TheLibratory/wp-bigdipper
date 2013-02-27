158f4dbf8e920386df234f4bc26cd1f38bc534f7
Casey Hunt <casey.hunt@gmail.com>
2013-02-27 12:40:35 -0600

    Added footer support- minor CSS changes.

--- footer.php
+++ footer.php
@@ -24,7 +24,14 @@
 
 			<div id="site-generator">
 				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+				<ul id="credits">
+					<li><a href="<?php echo esc_url( __( 'http://blogs.lib.utexas.edu', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'University of Texas Libraries Blogs Member Site', 'twentyeleven' ); ?>"><?php printf( __( 'LIBblog', 'twentyeleven' ), 'WordPress' ); ?></a><?php echo __( ' member site' ); ?></li>
+					<li><?php echo __( 'powered by ' ); ?><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>"><?php printf( __( ' %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+					</li>
+					<li><?php echo __( 'running') ?> Ursa Theme by <a href="http://blogs.lib.utexas.edu/tis/">The Libratory</a></li>
+					<li><?php echo __( 'built on ' ); ?><a href="<?php echo esc_url( __( 'http://theme.wordpress.com/themes/twentyeleven/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Twenty Eleven Theme by Wordpress.com', 'twentyeleven' ); ?>"><?php printf( __( 'TwentyEleven' ), 'WordPress' ); ?></a></li>
+					<li><?php echo __( 'social icons by ' ); ?><a href="<?php echo esc_url( __( 'http://www.fontfabric.com', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Fontfabric', 'twentyeleven' ); ?>"><?php printf( __( 'Fontfabric' ), 'WordPress' ); ?></a></li>
+				</ul>
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
@@ -35,9 +42,9 @@
 	UT & Library Branding 
 ****************************-->
 	<div id="utlib-branding-bar">
-				<div class="branding-bar bar-ut"><img src="<?php echo get_stylesheet_directory_uri()?>/images/utlib-footer-tower.jpg"></div>
+				<div class="branding-bar bar-ut"><img src="<?php bloginfo('stylesheet_directory')?>/images/utlib-footer-tower.jpg"></div>
 				<div class="branding-bar bar-library">
-					<div id="bar-liblogo"><img src="<?php echo get_stylesheet_directory_uri()?>/images/utlib-footer-flyingbook.gif"></div>
+					<div id="bar-liblogo"><img src="<?php bloginfo('stylesheet_directory')?>/images/utlib-footer-flyingbook.gif"></div>
 						<div class="bar-metabox">
 							<div class="bar-metabox blogin"><a href="wp-admin/">Log In/Admin</a></div>
 							<div class="bar-metabox libblog"><a href="#">LIBblog</a></div>
