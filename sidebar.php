69952053069a1d1da637efd4a3dadc983105549a
Casey Hunt <casey.hunt@gmail.com>
2013-04-11 15:49:21 -0500

    Responsive tricks added to break sidebar widgets into columns at low horizontal res.

--- sidebar.php
+++ sidebar.php
@@ -35,13 +35,13 @@ if ( 'content' != $current_layout ) :
 				<aside id="lib-social" class="widget">
 					<h3 class="widget-title"><?php _e( 'Follow UT Libraries', 'twentyeleven' ); ?></h3>
 					<ul class="liblinks-list libsocial">
-						<li><a href="http://plus.google.com/117391561174028073964/" data-icon="g">Google+</a></li>
-						<li><a href="http://www.twitter.com/utlibraries" data-icon="t">Twitter</a></li>
-						<li><a href="http://www.facebook.com/utlibraries" data-icon="f">Facebook</a></li>
-						<li><a href="http://www.flickr.com/utlibraries" data-icon="r">Flickr</a></li>
-						<li><a href="http://www.youtube.com/utlibraries" data-icon="y">YouTube</a></li>
-						<li><a href="http://www.friendfeed.com/utlibraries" data-icon="d">Friendfeed</a></li>
-						<li><a href="http://pinterest.com/utlibraries/" data-icon="p">Pinterest</a></li></ul>
+						<li><a href="http://plus.google.com/117391561174028073964/" data-icon="g"><span class="social-text">Google+</span></a></li>
+						<li><a href="http://www.twitter.com/utlibraries" data-icon="t"><span class="social-text">Twitter</span></a></li>
+						<li><a href="http://www.facebook.com/utlibraries" data-icon="f"><span class="social-text">Facebook</span></a></li>
+						<li><a href="http://www.flickr.com/utlibraries" data-icon="r"><span class="social-text">Flickr</span></a></li>
+						<li><a href="http://www.youtube.com/utlibraries" data-icon="y"><span class="social-text">YouTube</span></a></li>
+						<li><a href="http://www.friendfeed.com/utlibraries" data-icon="d"><span class="social-text">Friendfeed</span></a></li>
+						<li><a href="http://pinterest.com/utlibraries/" data-icon="p"><span class="social-text">Pinterest</span></a></li></ul>
 				</aside>				
 		</div><!-- #secondary .widget-area -->
 <?php endif; ?>
